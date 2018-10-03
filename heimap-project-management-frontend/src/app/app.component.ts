5721869ecfd8171607284c1520224c68616c1df3
Mohammed Zia <mohammed.zia33@gmail.com>
2018-10-03 15:48:52 +0200

    create project done

--- heimap-project-management-frontend/src/app/app.component.ts
+++ heimap-project-management-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { Router } from '@angular/router';
+import { Router, Data } from '@angular/router';
+import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,15 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   
-  if_new_project = false;
+  // @Input() input_username: String;
+  username: String = "";
+  auth_key: String = "";
+  proj_name: String = "";
+  organization: String = "";
+  funder: String = "";
+  description: String = "";
+  auth_key_validity: Boolean = false;
+  if_new_project: Boolean = false; // Default false
 
   boolNewProjectToggle = function(){    
     this.if_new_project ==  false ? this.if_new_project =  true : this.if_new_project =  false;
@@ -22,5 +31,48 @@ export class AppComponent {
   btnNewProjClick= function () {
     this.router.navigateByUrl('/user');
   };
+
+  usernameInput = function(event: any){
+    // SQL Check if username exists
+    ////////////////////
+    if(event.target.classList.contains('input-error')){
+      event.target.classList.remove('input-error');
+      event.target.classList.add('input-success');
+    } else if (event.target.classList.contains('input-success')){
+      event.target.classList.remove('input-success');
+      event.target.classList.add('input-error');
+    }
+  }
+
+  authKeyInput = function(event: any){
+    // SQL Check if auth_key for corresponding username valid
+    this.auth_key_validity = true; // Make it true if everything is ok and valid for auth_key and username
+    ////////////////////
+    if(event.target.classList.contains('input-error')){
+      event.target.classList.remove('input-error');
+      event.target.classList.add('input-success');
+    } else if (event.target.classList.contains('input-success')){
+      event.target.classList.remove('input-success');
+      event.target.classList.add('input-error');
+    }
+  }
+
+  boolSubmitProject = function(){
+    if (this.proj_name != null && this.auth_key_validity){
+      return false;
+    } else{
+      return true;
+    }
+  }
+
+  createNewProject = function(){
+    // Create New Project - drupal api and invalidate username and auth_key
+    ////////////////////
+    this.boolNewProjectToggle();
+  }
+
+  successProjectCreate = function(){
+    alert("Project Created Successfully!");
+  }
 
 }
--- heimap-project-management-frontend/src/app/app.module.ts
+++ heimap-project-management-frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NewProjectComponent } from './src/new-project/new-project.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
 
 const appRoutes: Routes = [
   // { path: '', component: DomComponent},
@@ -18,6 +20,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    FormsModule,
+    BootstrapModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
